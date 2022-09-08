69d4b4502518048d3ec2ba93639750f59d50d19a
Charles Phil <71610407+charlesphil@users.noreply.github.com>
2022-09-08 10:13:29 -0400

    wuah

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+function Page() {
+  return (
+    <div>
+      <h1>Reasons why I'm excited to learn React:</h1>
+      <ol>
+        <li>It's new!</li>
+        <li>It's exciting!</li>
+        <li>Will this make me a better programmer?</li>
+      </ol>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const img = (
-  <img src="logo512.png" alt="React logo"></img>
-);
-
-root.render(img);
+root.render(<Page />);
